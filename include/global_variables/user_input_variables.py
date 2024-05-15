3d7177a3e18caa7410ffaacf905b4842a5987c4b
Alejandra Sisinni <alejandra.sisinni@gmail.com>
2024-05-15 17:32:52 +0200

    Adding information

---
+++ include/global_variables/user_input_variables.py
@@ -0,0 +1,8 @@
+# -------------------- #
+# Enter your own info! #
+# -------------------- #
+
+MY_NAME = "Alejandra"
+MY_CITY = "Radolfzell"
+HOT_DAY = 30 # modify what you consider a hot day in degree Celsius
+BIRTH_YEAR = 1990
